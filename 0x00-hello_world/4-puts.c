af9c8ce67876144cca30df0ccb33a7a6e0dcefc3
Tamicachy <chidiogoezimora@gmail.com>
2023-09-07 16:05:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdioh>
+
+/**
+ * main - this is the main put function 
+ * *Return: 0
+ * */
+int main(void)
+{
+	pute("\"programming is the like building a multilingual puzzle");
+	return (0);
+}
